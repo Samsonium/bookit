57ec979a503d2b2cbe555fa6f639d098931affc3
Samsonium <tdesu@vk.com>
2023-05-07 02:53:57 +0300

    Created test for @Router decorator

--- test/decorators/router.spec.ts
+++ test/decorators/router.spec.ts
@@ -1,6 +1,22 @@
-import Router from '../../src/decorators/router';
+import { Router, getRouterMeta } from '../../src/decorators';
 
 describe('decorators/Router', () => {
 	it('Has type "function"', () => expect(typeof Router).toBe('function'));
 	it('Has return type "function"', () => expect(typeof Router()).toBe('function'));
+	it('Has metadata on use', () => {
+		@Router()
+		class TestClass {}
+		
+		const classMeta = getRouterMeta(TestClass);
+		expect(Object.keys(classMeta)).toContain('prefix');
+	});
+	it('Metadata sets correctly', () => {
+		@Router()
+		class A {}
+		expect(getRouterMeta(A).prefix).toBe('/');
+		
+		@Router('some-prefix')
+		class B {}
+		expect(getRouterMeta(B).prefix).toBe('/some-prefix');
+	});
 });
