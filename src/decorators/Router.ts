87bd64060fc4733c5cb8f922771e31b5bc65c098
Samsonium <tdesu@vk.com>
2023-05-07 02:55:05 +0300

    Router decorator renamed to match the function name

---
+++ src/decorators/Router.ts
@@ -0,0 +1,29 @@
+import Metadata from './interfaces/Metadata';
+
+/** Creates a new router */
+export default function Router(prefix?: string) {
+	return <T extends { new(...args: any[]): any }>(base: T) => {
+		return class extends base {
+			public meta: Metadata;
+			
+			constructor(...args: any[]) {
+				super(...args);
+				
+				const prefixChecked = prefix?.startsWith('/')
+					? prefix?.trim() : '/' + (prefix?.trim() ?? '');
+				
+				const paths = base.prototype['paths'];
+				
+				this.meta = {
+					prefix: prefixChecked,
+					paths: paths ?? []
+				};
+			}
+		};
+	};
+}
+
+/** Get router metadata */
+export function getRouterMeta(router: any): Metadata {
+	return (new router()).meta satisfies Metadata;
+}
--- src/decorators/router.ts
+++
@@ -1,3 +0,0 @@
-export default function Router(): ClassDecorator {
-	return (target) => {/**/};
-}
