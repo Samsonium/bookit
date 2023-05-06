5534931fca54b7143a2125cc3d407523c39210c6
Samsonium <tdesu@vk.com>
2023-05-06 16:12:07 +0300

    Calls to jest.spyOn are taken out outside the test functions

--- test/logger.spec.ts
+++ test/logger.spec.ts
@@ -2,37 +2,29 @@ import Logger from '../src/utils/Logger';
 import LogType from '../src/utils/enums/LogType';
 
 describe('Logger class', () => {
+	
+	// Spy on console.log
+	let spy: jest.SpyInstance;
+	beforeEach(() => (spy = jest.spyOn(console, 'log')));
+	afterEach(() => spy.mockRestore());
+	
+	// Method: Logger.out
+	
 	test('Has "out" static method', () => expect(typeof Logger.out).toBe('function'));
 	test('Has "out" is callable', () => {
-		const spy = jest.spyOn(console, 'log').mockImplementation();
-		
 		Logger.out(LogType.info, 'Hello,', 'World!');
 		expect(spy.mock.calls[0].join(' ')).toContain('Hello, World!');
-		
-		spy.mockRestore();
 	});
 	test('Has "out(info)" works properly', () => {
-		const spy = jest.spyOn(console, 'log').mockImplementation();
-		
 		Logger.out(LogType.info, '1');
 		expect(spy.mock.calls[0][0]).toContain('INF');
-		
-		spy.mockRestore();
 	});
 	test('Has "out(warning)" works properly', () => {
-		const spy = jest.spyOn(console, 'log').mockImplementation();
-		
 		Logger.out(LogType.warning, '1');
 		expect(spy.mock.calls[0][0]).toContain('WRN');
-		
-		spy.mockRestore();
 	});
 	test('Has "out(error)" works properly', () => {
-		const spy = jest.spyOn(console, 'log').mockImplementation();
-		
 		Logger.out(LogType.error, '1');
 		expect(spy.mock.calls[0][0]).toContain('ERR');
-		
-		spy.mockRestore();
 	});
 });
