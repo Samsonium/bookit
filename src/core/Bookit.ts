0495c98c6a1a181e7157c12c79de0b9581d494fa
Samsonium <tdesu@vk.com>
2023-05-07 04:34:38 +0300

    Implemented routers adding and start/stop methods

--- src/core/Bookit.ts
+++ src/core/Bookit.ts
@@ -1,6 +1,9 @@
 import { Server } from 'http';
 import HttpWrapper from './HttpWrapper';
 import HttpWrapperConfig from '../utils/interfaces/HttpWrapperConfig';
+import Metadata from '../utils/interfaces/Metadata';
+import Logger from '../utils/Logger';
+import LogType from '../utils/enums/LogType';
 
 /** BOOKit server class */
 export default class Bookit {
@@ -16,8 +19,71 @@ export default class Bookit {
 	}
 	
 	/** Define routers */
-	public addRouters(...routers: { new(...args: any[]): any[] }[]): void {
-		//
+	public addRouters(...routers: { new(...args: any[]): any }[]): void {
+		for (const router of routers) {
+			const instance = new router();
+			const meta = instance.meta as Metadata;
+			
+			if (!meta) {
+				Logger.out(LogType.warning, 'Router', router.name, 'doesn\'t have metadata');
+				continue;
+			}
+			
+			for (const path of meta.paths) {
+				let prefix = meta.prefix.trim();
+				if (!prefix.startsWith('/')) prefix = '/' + prefix;
+				if (prefix.charAt(prefix.length - 1) === '/')
+					prefix = prefix.substring(0, prefix.length - 1);
+				
+				let pathname = path.path.trim();
+				if (!pathname.startsWith('/')) pathname = '/' + pathname;
+				
+				const pathParts = pathname.split('/').filter(Boolean);
+				
+				// Register request
+				this.httpWrapper.onRequest((req, res) => {
+					if (req.method !== path.method) return;
+					
+					const url = new URL(req.url);
+					const urlParts = url.pathname.split('/').filter(Boolean);
+					if (urlParts.length !== pathParts.length) return;
+					
+					const params: {
+						name: string,
+						value: string,
+					}[] = [];
+					
+					for (let i = 0; i < urlParts.length; i++) {
+						const inUrl = urlParts[i];
+						const inPath = pathParts[i];
+						if (inPath.startsWith(':')) {
+							params.push({
+								name: inPath.substring(1),
+								value: inUrl
+							});
+							continue;
+						}
+						if (inUrl !== inPath) return;
+					}
+					
+					// All ok, call method
+					instance[path.executor]({
+						request: { ...req, params: params },
+						response: res
+					});
+				});
+			}
+		}
+	}
+	
+	/** Start server */
+	public get start(): HttpWrapper['start'] {
+		return this.httpWrapper.start;
+	}
+	
+	/** Stop server */
+	public get stop(): HttpWrapper['stop'] {
+		return this.httpWrapper.stop;
 	}
 	
 	/** http.Server instance getter */
--- test/core/bookit.spec.ts
+++ test/core/bookit.spec.ts
@@ -1,13 +1,33 @@
 import Bookit from '../../src/core/Bookit';
 import { Server } from 'http';
+import { Router, Get } from '../../src/decorators';
 
 describe('core/Bookit', () => {
 	it('Can initialize', () => expect(new Bookit).toBeInstanceOf(Bookit));
 	it('Has "addRouters" method to use routers', () => {
 		expect(typeof new Bookit().addRouters).not.toBe(undefined);
 	});
+	it('Has "start" method', () => {
+		expect(typeof new Bookit().start).not.toBe(undefined);
+	});
+	it('Has "stop" method', () => {
+		expect(typeof new Bookit().stop).not.toBe(undefined);
+	});
 	it('Has "server" field (http.Server instance)', () => {
 		const kit = new Bookit();
 		expect(kit.server).toBeInstanceOf(Server);
+	});
+	it('Routers can add', () => {
+		const kit = new Bookit();
+		expect(kit.server.listenerCount('request')).toBe(0);
+		
+		@Router()
+		class SomeRouter {
+			@Get()
+			helloWorld() {/**/}
+		}
+		
+		kit.addRouters(SomeRouter);
+		expect(kit.server.listenerCount('request')).toBe(1);
 	});
 });
