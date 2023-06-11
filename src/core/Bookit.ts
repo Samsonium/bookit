faef413179cd5a88e3ccf4afbe133742bf09dbae
Samsonium <124593895+Samsonium@users.noreply.github.com>
2023-06-11 17:49:31 +0300

    Fix for #16 and #21

--- src/core/Bookit.ts
+++ src/core/Bookit.ts
@@ -1,19 +1,20 @@
-import {Server} from 'http';
+import { Server } from 'http';
 import HttpWrapper from './HttpWrapper';
 import HttpWrapperConfig from '../utils/interfaces/HttpWrapperConfig';
 import Metadata from '../utils/interfaces/Metadata';
 import Logger from '../utils/Logger';
 import LogType from '../utils/enums/LogType';
+import * as path from 'path';
 
 /** BOOKit server class */
 export default class Bookit {
-	
+
 	/** HttpWrapper instance */
 	public readonly httpWrapper: HttpWrapper;
-	
+
 	public constructor();
 	public constructor(config: Partial<HttpWrapperConfig>);
-	
+
 	public constructor(config?: Partial<HttpWrapperConfig>) {
 		let httpConfig: HttpWrapperConfig = {
 			logs: false,
@@ -21,18 +22,18 @@ export default class Bookit {
 				timeout: false,
 				bodyLimit: 1048576,
 				caseSensitive: true,
-				ignoreTrailingSlash: false
+				ignoreTrailingSlash: false,
 			},
 			response: {
-				timeout: false
-			}
+				timeout: false,
+			},
 		};
-		
+
 		if (config) httpConfig = Object.assign(httpConfig, config);
-		
+
 		this.httpWrapper = new HttpWrapper(httpConfig);
 	}
-	
+
 	/**
 	 * Define routers
 	 * @param routers Routers to be active
@@ -41,93 +42,100 @@ export default class Bookit {
 		for (const router of routers) {
 			const instance = new router();
 			const meta = instance.meta as Metadata;
-			
+
 			if (!meta) {
 				Logger.out(LogType.warning, 'Router', router.name, 'doesn\'t have metadata');
 				continue;
 			}
-			
-			for (const path of meta.paths) {
-				let prefix = meta.prefix.trim();
-				if (!prefix.startsWith('/')) prefix = '/' + prefix;
-				if (prefix.charAt(prefix.length - 1) === '/')
-					prefix = prefix.substring(0, prefix.length - 1);
-				
-				let pathname = path.path.trim();
-				if (!pathname.startsWith('/')) pathname = '/' + pathname;
-				
-				const pathParts = pathname.split('/').filter(Boolean);
-				
-				// Register request
-				this.httpWrapper.onRequest((req, res) => {
-					if (req.method !== path.method) return;
 
-					const urlParts = req.url.split('/').filter(Boolean);
-					if (urlParts.length !== pathParts.length) return;
-					
-					const params: {
-						[name: string]: string,
-					} = {};
-					
-					for (let i = 0; i < urlParts.length; i++) {
-						const inUrl = urlParts[i];
-						const inPath = pathParts[i];
-						if (inPath.startsWith(':')) {
-							params[inPath.substring(1)] = inUrl;
-							continue;
-						}
-						if (inUrl !== inPath) return;
-					}
-					
-					// All ok, call method
-					const result = instance[path.executor]({
-						request: { ...req, params },
-						response: res
-					});
-
-					res.writeHead(200);
-					switch (typeof result) {
-					case 'object':
-						res.setHeader('Content-Type', 'application/json');
-						res.write(JSON.stringify(result));
-						res.end();
-						break;
-
-					case 'boolean':
-					case 'number':
-					case 'symbol':
-					case 'bigint':
-					case 'string':
-						res.write(String(result));
-						res.end();
-						break;
-
-					case 'function':
-						res.writeHead(500);
-						res.end(() => {
-							throw new Error('Cannot write out function');
-						});
-						break;
-
-					case 'undefined':
-						res.writeHead(200);
-						res.end();
-					}
-				});
-			}
+			for (const path of meta.paths)
+				this.makeRouteHandler(path, instance);
 		}
 	}
-	
+
+	/**
+	 * Prepare full pathname
+	 * @param prefix Router prefix
+	 * @param pathname Path to handling function
+	 * @private
+	 */
+	private prepareRoutePath(prefix: string, pathname: string): string {
+		prefix = prefix.trim();
+		pathname = pathname.trim();
+
+		// Check for start slash
+		if (!/\/(.+)?/.test(prefix))
+			prefix = '/' + prefix;
+
+		// Check for trailing slash
+		if (/.+\//.test(prefix)) {
+			if (/\/(.+)?/.test(pathname))
+				pathname = pathname.substring(0, pathname.length - 1);
+			else pathname = '/' + pathname;
+		}
+
+		return prefix + pathname;
+	}
+
+	/**
+	 * Creates route handler
+	 * @param path
+	 * @param router
+	 * @private
+	 */
+	private makeRouteHandler(path: Metadata['paths'][number], router: any): void {
+		const expected = this.prepareRoutePath(router.meta.prefix, path.path)
+			.split('/').filter(Boolean);
+
+		// Register request
+		this.httpWrapper.onRequest((req, res) => {
+			if (req.method !== path.method) return;
+
+			// Get requested pathname
+			const got = req.url.split('/').filter(Boolean);
+			if (got.length !== expected.length) return;
+
+			// Create params list
+			const params: {
+				[name: string]: string,
+			} = {};
+
+			for (let i = 0; i < got.length; i++) {
+				if (!expected[i].startsWith(':') && got[i] !== expected[i]) return;
+				else if (expected[i].startsWith(':'))
+					params[expected[i].substring(1)] = got[i];
+			}
+
+			// All ok, call method
+			const result = router[path.executor]({
+				request: { ...req, params },
+				response: res,
+			});
+
+			// Set status if not set
+			if (!res.statusCode)
+				res.statusCode = 200;
+
+			// Check for handler returned value type
+			if (typeof result === 'object') {
+				res.setHeader('Content-Type', 'application/json');
+				res.end(JSON.stringify(result));
+			} else if (typeof result === 'function') {
+				throw new Error('Cannot write out function');
+			} else res.end(result);
+		});
+	}
+
 	/** Start server */
 	public get start(): HttpWrapper['start'] {
 		return this.httpWrapper.start;
 	}
-	
+
 	/** Stop server */
 	public get stop(): HttpWrapper['stop'] {
 		return this.httpWrapper.stop;
 	}
-	
+
 	/** http.Server instance getter */
 	public get server(): Server {
 		return this.httpWrapper.server;
